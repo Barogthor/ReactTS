482e6d5af71ad2b196b403d0ed344bc90bceadfd
Barogthor <f.thery3103@gmail.com>
2019-07-25 11:35:52 +0200

    config, style

--- src/index.tsx
+++ src/index.tsx
@@ -4,31 +4,15 @@ import * as ReactDOM from "react-dom";
 import thunkMiddleware from 'redux-thunk'
 import App from "./components/App";
 import {applyMiddleware, createStore} from "redux";
-import appReducer from "./reducers/appReducer";
 import Root from "./components/Root";
+import {todoReducer} from "./store/Todo/reducers";
+import configureStore from "./store";
 
-const store = createStore(
-    appReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        // loggerMiddleware
-    )
-)
+const store = configureStore()
 
 ReactDOM.render(
     <Root store={store} />,
     document.getElementById('root')
 );
 
-
-const c1 = 0;
-let v = "";
-
-const arr1 = [1,2,3,4];
-const arr = [...arr1, 5,6,7,47];
-
-const print = (s) => console.log(s);
-
-print("hello world");
-console.table(arr);
-document.write("Je débute avec Webpack !!!!!");
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
